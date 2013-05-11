21f80e585b57425a8ff40c81d4cfd0ad874ff1b5
Tray Torrance <devwork@warrentorrance.com>
2013-05-10 22:52:15 -0700

    Remove non-functional parameters from top-level command

--- lib/starter_kit/cmd/main.rb
+++ lib/starter_kit/cmd/main.rb
@@ -3,9 +3,6 @@ require 'starter_kit/command'
 Dir["#{File.dirname(__FILE__)}/*.rb"].each { |f| require f }
 
 class StarterKit::Cmd::Main < Clamp::Command
-  option ['-a', '--author'], 'AUTHOR', 'The name of the author'
-  option ['-e', '--email'],  'EMAIL',  'The email address of the author'
-
   subcommand 'puppet', 'Create a puppet module', StarterKit::Cmd::Puppet 
 end
 
